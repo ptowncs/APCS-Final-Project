11ee1f9e2c4ac66846d533efaf23dbabcbb1b8c2
ElectricSplash <ravi.ramamoorthy2706@gmail.com>
2026-05-27 00:21:17 -0700

    idk

--- Main.java
+++ Main.java
@@ -1,140 +1,246 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Main{
     public static Scanner scanner = new Scanner(System.in);
     public static Player player = new Player(100);
-    public static NonPlayer bandit = new NonPlayer("Dagger", 100, 0.5, 0.3, new ArrayList<>());
-    public static NonPlayer mobBoss = new NonPlayer("Longsword", 125, 0.6, 0.4, new ArrayList<>());
-    public static NonPlayer demonCaptain = new NonPlayer("Spear", 150, 0.65, 0.45, new ArrayList<>());
-    public static NonPlayer demonGeneral = new NonPlayer("War Hammer", 200, 0.7, 0.5, new ArrayList<>());
-    public static NonPlayer demonKing = new NonPlayer("Poleaxe", 300, 0.75, 0.2, new ArrayList<>());
+    public static NonPlayer bandit = new NonPlayer("Dagger", 75, 0.5, 0.3, new ArrayList<>());
+    public static NonPlayer mobBoss = new NonPlayer("Longsword", 100, 0.6, 0.4, new ArrayList<>());
+    public static NonPlayer demonCaptain = new NonPlayer("Spear", 125, 0.65, 0.45, new ArrayList<>());
+    public static NonPlayer demonGeneral = new NonPlayer("War Hammer", 175, 0.7, 0.5, new ArrayList<>());
+    public static NonPlayer demonKing = new NonPlayer("Poleaxe", 275, 0.75, 0.2, new ArrayList<>());
+
     public static boolean banditBattle(){
         while(player.getHealth() > 0 && bandit.getHealth() > 0){
             System.out.println("What will you do? 1. Attack 2. Switch Weapon");
-             int choice = scanner.nextInt();
-             scanner.nextLine(); 
-             if(choice == 1){
-                 if(player.attack(bandit)){
-                     System.out.println("Hit, Bandit health: " + bandit.getHealth());
-                 } else {
-                     System.out.println("Miss!");
-                 }
-             } else {
-                 System.out.println("Bandit is next!");
-             }
-             if (bandit.getHealth() <= 0) break;
-             if(bandit.attack(player, Math.random())){
-                 System.out.println("Bandit hit, Your health: " + player.getHealth());
-             } else {
-                 System.out.println("Bandit missed!");
-             }
-           
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+            if(choice == 1){
+                if(player.attack(bandit)){
+                    System.out.println("Hit, Bandit health: " + bandit.getHealth());
+                } else {
+                    System.out.println("Miss!");
+                }
+            } else if(choice == 2) {
+                System.out.println("\n--- Choose a Weapon ---");
+                System.out.println("1. Dagger");
+                System.out.print("Select your weapon: ");
+                int weaponChoice = scanner.nextInt();
+                scanner.nextLine();
+                if (weaponChoice == 1) {
+                    player.changeWeapon("Dagger");
+                    System.out.println("You equipped the Dagger!");
+                } else {
+                    System.out.println("Invalid selection!");
+                }
+            }
+            if (bandit.getHealth() <= 0) break;
+            if(bandit.attack(player, Math.random())){
+                System.out.println("Bandit hit, Your health: " + player.getHealth());
+            } else {
+                System.out.println("Bandit missed!");
+            }
         }
         return player.getHealth() > 0;
+    }
+
+    public static boolean mobBossBattle() {
+        while (player.getHealth() > 0 && mobBoss.getHealth() > 0) {
+            System.out.println("\nWhat will you do? 1. Attack 2. Switch Weapon");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+
+            if (choice == 1) {
+                if (player.attack(mobBoss)) {
+                    System.out.println("Hit, Mob Boss health: " + mobBoss.getHealth());
+                } else {
+                    System.out.println("Miss!");
+                }
+            } else if (choice == 2) {
+                System.out.println("\n--- Choose a Weapon ---");
+                System.out.println("1. Dagger");
+                System.out.println("2. Longsword");
+                System.out.print("Select your weapon: ");
+                int weaponChoice = scanner.nextInt();
+                scanner.nextLine();
+
+                if (weaponChoice == 1) {
+                    player.changeWeapon("Dagger");
+                    System.out.println("Switched to Dagger!");
+                } else if (weaponChoice == 2) {
+                    player.changeWeapon("Longsword");
+                    System.out.println("Switched to Longsword!");
+                } else {
+                    System.out.println("You fumbled your choice.");
+                }
+            }
+
+            if (mobBoss.getHealth() <= 0) break;
+
+            if (mobBoss.attack(player, Math.random())) {
+                System.out.println("Mob Boss hit, Your health: " + player.getHealth());
+            } else {
+                System.out.println("Mob Boss missed!");
+            }
         }
-        public static boolean mobBossBattle(){
-            while(player.getHealth() > 0 && mobBoss.getHealth() > 0){
+        return player.getHealth() > 0;                                                   
+    }
+
+    public static boolean demonCaptainBattle(){
+        while(player.getHealth() > 0 && demonCaptain.getHealth() > 0){
             System.out.println("What will you do? 1. Attack 2. Switch Weapon");
-             int choice = scanner.nextInt();
-            scanner.nextLine();; // Placeholder for user input
-             if(choice == 1){
-                 if(player.attack(mobBoss)){
-                     System.out.println("Hit, Mob Boss health: " + mobBoss.getHealth());
-                 } else {
-                     System.out.println("Miss!");
-                 }
-                }
-                    if (mobBoss.getHealth() <= 0) break;
-                if(mobBoss.attack(player, Math.random())){
-                    System.out.println("Mob Boss hit, Your health: " + player.getHealth());
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+            if(choice == 1){
+                if(player.attack(demonCaptain)){
+                    System.out.println("Hit, Demon Captain health: " + demonCaptain.getHealth());
                 } else {
-                    System.out.println("Mob Boss missed!");
+                    System.out.println("Miss!");
+                }
+            } else if(choice == 2){
+                System.out.println("\n--- Choose a Weapon ---");
+                System.out.println("1. Dagger");
+                System.out.println("2. Longsword");
+                System.out.println("3. Spear");
+                System.out.print("Select your weapon: ");
+                int weaponChoice = scanner.nextInt();
+                scanner.nextLine();
+
+                if (weaponChoice == 1) {
+                    player.changeWeapon("Dagger");
+                    System.out.println("You equipped the Dagger!");
+                } else if (weaponChoice == 2) {
+                    player.changeWeapon("Longsword");
+                    System.out.println("You equipped the Longsword!");
+                } else if (weaponChoice == 3) {
+                    player.changeWeapon("Spear");
+                    System.out.println("You equipped the Spear!");
+                } else {
+                    System.out.println("Invalid selection!");
                 }
             }
-        
-            return player.getHealth() > 0;                                                  
-        }
-        public static boolean demonCaptainBattle(){
-            while(player.getHealth() > 0 && demonCaptain.getHealth() > 0){
-                 System.out.println("What will you do? 1. Attack 2. Switch Weapon");
-             int choice = scanner.nextInt();
-             scanner.nextLine(); // Placeholder for user input
-             if(choice == 1){
-                 if(player.attack(demonCaptain)){
-                     System.out.println("Hit, Demon Captain health: " + demonCaptain.getHealth());
-                 } else {
-                     System.out.println("Miss!");
-                 }
-                }
-                if(demonCaptain.attack(player, Math.random())){
-                    System.out.println("Demon Captain hit, Your health: " + player.getHealth());
-                } else {
-                    System.out.println("Demon Captain missed!");
-                }
+            
+            if (demonCaptain.getHealth() <= 0) break;
+            
+            if(demonCaptain.attack(player, Math.random())){
+                System.out.println("Demon Captain hit, Your health: " + player.getHealth());
+            } else {
+                System.out.println("Demon Captain missed!");
             }
-            return player.getHealth() > 0;
         }
-        public static boolean demonGeneralBattle(){
-            while(player.getHealth() > 0 && demonGeneral.getHealth() > 0){
-                 System.out.println("What will you do? 1. Attack 2. Switch Weapon");
-             int choice = scanner.nextInt();
-            scanner.nextLine();; // Placeholder for user input
-             if(choice == 1){
-                 if(player.attack(demonGeneral)){
-                     System.out.println("Hit, Demon General health: " + demonGeneral.getHealth());
-                 } else {
-                     System.out.println("Miss!");
-                 }
-                }else if(choice == 2){
-                    player.changeWeapon("");
-                    System.out.println("Demon General is next!");
-                }
-                if(demonGeneral.attack(player, Math.random())){
-                    System.out.println("Demon General hit, Your health: " + player.getHealth());
-                } else {
-                    System.out.println("Demon General missed!");
-                }
-                if(choice == 2){
-                    player.changeWeapon("");
-                    System.out.println("Demon General is next!");
-                }
-            }
-            return player.getHealth() > 0;
-        }
-        public static boolean demonKingBattle(){
-            while(player.getHealth() > 0 && demonKing.getHealth() > 0){
+        return player.getHealth() > 0;
+    }
+
+    public static boolean demonGeneralBattle(){
+        while(player.getHealth() > 0 && demonGeneral.getHealth() > 0){
             System.out.println("What will you do? 1. Attack 2. Switch Weapon");
-             int choice = scanner.nextInt();
-             scanner.nextLine(); // Placeholder for user input
-             if(choice == 1){
-                 if(player.attack(demonKing)){
-                     System.out.println("Hit, Demon King health: " + demonKing.getHealth());
-                 } else {
-                     System.out.println("Miss!");
-                 }
-                }else if(choice == 2){
-                    player.changeWeapon("");
-                    System.out.println("Demon King is next!");
-                }
-                 if (demonKing.getHealth() <= 0) break;
-                if(demonKing.attack(player, Math.random())){
-                    System.out.println("Demon King hit, Your health: " + player.getHealth());
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+            if(choice == 1){
+                if(player.attack(demonGeneral)){
+                    System.out.println("Hit, Demon General health: " + demonGeneral.getHealth());
                 } else {
-                    System.out.println("Demon King missed!");
+                    System.out.println("Miss!");
+                }
+            } else if(choice == 2){
+                System.out.println("\n--- Choose a Weapon ---");
+                System.out.println("1. Dagger");
+                System.out.println("2. Longsword");
+                System.out.println("3. Spear");
+                System.out.println("4. War Hammer");
+                System.out.print("Select your weapon: ");
+                int weaponChoice = scanner.nextInt();
+                scanner.nextLine();
+
+                if (weaponChoice == 1) {
+                    player.changeWeapon("Dagger");
+                    System.out.println("You equipped the Dagger!");
+                } else if (weaponChoice == 2) {
+                    player.changeWeapon("Longsword");
+                    System.out.println("You equipped the Longsword!");
+                } else if (weaponChoice == 3) {
+                    player.changeWeapon("Spear");
+                    System.out.println("You equipped the Spear!");
+                } else if (weaponChoice == 4) {
+                    player.changeWeapon("War Hammer");
+                    System.out.println("You equipped the War Hammer!");
+                } else {
+                    System.out.println("Invalid selection!");
                 }
             }
-        
-            return player.getHealth() > 0;
-        
-    
+
+            if (demonGeneral.getHealth() <= 0) break;
+
+            if(demonGeneral.attack(player, Math.random())){
+                System.out.println("Demon General hit, Your health: " + player.getHealth());
+            } else {
+                System.out.println("Demon General missed!");
+            }
         }
+        return player.getHealth() > 0;
+    }
+
+    public static boolean demonKingBattle(){
+        while(player.getHealth() > 0 && demonKing.getHealth() > 0){
+            System.out.println("What will you do? 1. Attack 2. Switch Weapon");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+            if(choice == 1){
+                if(player.attack(demonKing)){
+                    System.out.println("Hit, Demon King health: " + demonKing.getHealth());
+                } else {
+                    System.out.println("Miss!");
+                }
+            } else if(choice == 2){
+                System.out.println("\n--- Choose a Weapon ---");
+                System.out.println("1. Dagger");
+                System.out.println("2. Longsword");
+                System.out.println("3. Spear");
+                System.out.println("4. War Hammer");
+                System.out.println("5. Poleaxe");
+                System.out.print("Select your weapon: ");
+                int weaponChoice = scanner.nextInt();
+                scanner.nextLine();
+
+                if (weaponChoice == 1) {
+                    player.changeWeapon("Dagger");
+                    System.out.println("You equipped the Dagger!");
+                } else if (weaponChoice == 2) {
+                    player.changeWeapon("Longsword");
+                    System.out.println("You equipped the Longsword!");
+                } else if (weaponChoice == 3) {
+                    player.changeWeapon("Spear");
+                    System.out.println("You equipped the Spear!");
+                } else if (weaponChoice == 4) {
+                    player.changeWeapon("War Hammer");
+                    System.out.println("You equipped the War Hammer!");
+                } else if (weaponChoice == 5) {
+                    player.changeWeapon("Poleaxe");
+                    System.out.println("You equipped the Poleaxe!");
+                } else {
+                    System.out.println("Invalid selection!");
+                }
+            }
+
+            if (demonKing.getHealth() <= 0) break;
+
+            if(demonKing.attack(player, Math.random())){
+                System.out.println("Demon King hit, Your health: " + player.getHealth());
+            } else {
+                System.out.println("Demon King missed!");
+            }
+        }
+        return player.getHealth() > 0;
+    }
 
     public static void main(String[] args){
+        int choice = 0;
         System.out.println("The king has chosen his warriors. The centerpiece of this group of warriors is you. you are sent on a mission. Defeat the demon king.");
         System.out.println("Standing among the ruins of a village, you are alone. your teammates are dead. You are facing a bandit");
         System.out.println("The battle begins!");
         if(banditBattle()){
             System.out.println("Congratulations! You have defeated the bandit");
+            player.setHealth(110);
         } else {
             System.out.println("You have been defeated. The world is doomed. Game Over.");
             return;
@@ -142,9 +248,17 @@ public class Main{
         System.out.println("You wander through the ruins and find a chest. Inside is a longsword. You equip it and continue on your journey.");
         player.addWeapon("Longsword", 0);
         System.out.println("you arrive to the forest of passage. A mob boss is blocking your path. He has a civilian in his clutches. 1. Attack and save the civilian 2. Sneak past the mob boss and continue");
+        choice = scanner.nextInt();
+        scanner.nextLine(); 
+        if(choice == 1){
+            System.out.println("You attack the mob boss to save the civilian");
+        } else {
+            System.out.println("You try to sneak past the mob boss, but he sees you and attacks!");
+        }
         System.out.println("battle begins!");
         if(mobBossBattle()){
             System.out.println("Congratulations! You have defeated the mob boss and saved the civilian");
+            player.setHealth(120);
         } else {
             System.out.println("You have been defeated. The world is doomed. Game Over.");
             return;
@@ -156,6 +270,7 @@ public class Main{
         System.out.println("Give em hell! ");
         if(demonCaptainBattle()){
             System.out.println("Congratulations! You have defeated the demon captain");
+            player.setHealth(150);
         } else {
             System.out.println("You have been defeated. The world is doomed. Game Over.");
             return;
@@ -167,6 +282,7 @@ public class Main{
         System.out.println("The Battle begins!");
         if(demonGeneralBattle()){
             System.out.println("Congratulations! You have defeated the demon general");
+            player.setHealth(200);
         } else {
             System.out.println("You have been defeated. The world is doomed. Game Over.");
             return;
